632b8ca299d37349d896b438dd69c8fb321b2612
ELIZA TAALAIBEK KYZY <mangoj0012@gmail.com>
2022-01-20 19:01:54 +0100

    Small changes

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
+from cgitb import grey
 import tkinter as tk;
-from tkinter import Button, Tk, filedialog
+from tkinter import Button, Label, Tk, filedialog
 from classification_emotions import Classifictions;
 import cv2
 from model import Model;
@@ -7,11 +8,6 @@ from PIL import Image, ImageTk
 
 model = Model.create_model()
 model.load_weights('./src/model_2022-01-08.h5')
-
-width, height = 800, 600
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
 
 class App:
@@ -42,19 +38,32 @@ class App:
         )
 
         # Positioning BUttons
-        self._select_btn.place(x=580, y=700, anchor=tk.N)
-        self._live_btn.place(x=580, y=730, anchor=tk.N)
-        self._exit_btn.place(x=580,y=760,anchor=tk.N)
+        self._select_btn.place(x=580, y=650, anchor=tk.N)
+        self._live_btn.place(x=580, y=680, anchor=tk.N)
+        self._exit_btn.place(x=580,y=710,anchor=tk.N)
 
         #Capture video frames
-        self.video_frame = tk.Label(self.window)
-        self.video_frame.grid(row=0, column=0)
+        self._decore_frame = tk.Label(self.window, bg="grey", width=100, height=30)
+        self._decore_frame.place(relx = 0.5,
+                              rely = 0.4,
+                              anchor = 'center')
+        self.video_frame = tk.Label(self.window, text = "Upload Image", bg="grey")
+        self.video_frame.place(relx = 0.5,
+                              rely = 0.4,
+                              anchor = 'center')
+
+        width = 800
+        height = 600
+        self.cap = cv2.VideoCapture(0)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
         self.window.mainloop()
         pass
 
     def live_face_expression(self):
-        _, img = cap.read()
+        self._decore_frame.destroy()
+        _, img = self.cap.read()
         img = cv2.flip(img, 1)
         
         Classifictions.get_expression_classified(img, model)
@@ -67,6 +76,7 @@ class App:
         self.video_frame.after(10, self.live_face_expression)
     
     def _load_image(self):
+        self._decore_frame.destroy()
         filenames = filedialog.askopenfilenames(
             title="Choose a file",
             filetypes=('image files', ('.png', '.jpg'))
@@ -80,14 +90,14 @@ class App:
                 width = int(img.shape[1])
                 height = int(img.shape[0])
                 scale = 100
-                while scale*width/100 >= 600 or scale*height/100 >= 800:
+                while scale*width/100 >= 800 or scale*height/100 >= 600:
                     scale=scale-1
                 width = int(img.shape[1] * scale / 100)
                 height = int(img.shape[0] * scale / 100)
 
                 # resize image
                 dim = (width, height)
-                img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+                img = cv2.resize(img, dim, fx=0.5, fy=0.4)
                 
                 Classifictions.get_expression_classified(img, model)
                 cv2image = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
